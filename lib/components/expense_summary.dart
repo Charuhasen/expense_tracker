cd99fb6f17adeecb85628d806cf2f0a798976da1
Charuhasen <charuhasen@gmail.com>
2023-02-19 15:21:44 +0000

    User can now add data to graph

--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -3,6 +3,8 @@ import 'package:expense_tracker/data/expense_data.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../datetime/date_time_helper.dart';
+
 class ExpenseSummary extends StatelessWidget {
   final DateTime startOfWeek;
   const ExpenseSummary({
@@ -13,19 +15,25 @@ class ExpenseSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Get the yymmdd
-    
+    String sunday = convertDateTimeToString(startOfWeek.add(const Duration(days: 0)));
+    String monday = convertDateTimeToString(startOfWeek.add(const Duration(days: 1)));
+    String tuesday = convertDateTimeToString(startOfWeek.add(const Duration(days: 3)));
+    String wednesday = convertDateTimeToString(startOfWeek.add(const Duration(days: 4)));
+    String thursday = convertDateTimeToString(startOfWeek.add(const Duration(days: 5)));
+    String friday = convertDateTimeToString(startOfWeek.add(const Duration(days: 6)));
+    String saturday = convertDateTimeToString(startOfWeek.add(const Duration(days: 7)));
     return Consumer<ExpenseData>(
       builder: (context, value, child) => SizedBox(
         height: 200,
         child: MyBarGraph(
           maxY: 100,
-          sunAmount: 20,
-          monAmount: 10,
-          tueAmount: 50,
-          wedAmount: 80,
-          thuAmount: 90,
-          friAmount: 30,
-          satAmount: 50,
+          sunAmount: value.calculateDailyExpenseSummary()[sunday] ?? 0,
+          monAmount: value.calculateDailyExpenseSummary()[monday] ?? 0,
+          tueAmount: value.calculateDailyExpenseSummary()[tuesday] ?? 0,
+          wedAmount: value.calculateDailyExpenseSummary()[wednesday] ?? 0,
+          thuAmount: value.calculateDailyExpenseSummary()[thursday] ?? 0,
+          friAmount: value.calculateDailyExpenseSummary()[friday] ?? 0,
+          satAmount: value.calculateDailyExpenseSummary()[saturday] ?? 0,
         ),
       ),
     );
